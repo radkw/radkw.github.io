b4fafc2f0f536ece8fc5ae1105ab17c1f73af5d7
radkw <radek.kwasniak@gmail.com>
2021-01-08 14:11:27 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-return 2^20
-var testing = document.querySelector
+var marcrophage = document.getElementById("macrophage");
+var virus = document.getElementById("virus");
+function up(){
+    if (macrophage.classList != "animate") {
+    macrophage.classList.add("animate");
+    }
+    setTimeout(function(){
+        macrophage.classList.remove("animate");
+    },500);
+}
+
+var checkDead = setInterval(function(){
+    var macrophageTop = 
+    parseInt(window.getComputedStyle(macrophage).getPropertyValue("top"));
+    var blockleft = parseInt(window.getComputedStyle(virus).getPropertyValue("left"));
+    if(blockleft<20 && blockleft>0 && characterTop>=130){
+        virus.style.animation = "none";
+        virus.style.display = "none";
+        alert("You got infected")
+    }
+},10);
